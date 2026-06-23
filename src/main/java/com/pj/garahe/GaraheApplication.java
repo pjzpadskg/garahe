84f91310e41ae64bde9e79e6fc7f9214014dd6d1
mlph-pjsantos <pauloletics@gmail.com>
2026-06-23 22:47:11 +0800

    chore: init

---
+++ src/main/java/com/pj/garahe/GaraheApplication.java
@@ -0,0 +1,13 @@
+package com.pj.garahe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaraheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaraheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pj/garahe/GaraheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.garahe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaraheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
